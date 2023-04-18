56ca1ac8fd96f7ac6b735f6b3b4197ef5d74eadb
Matt Q <neato0079@gmail.com>
2023-04-18 15:55:22 -0700

    Create a db called art_ref_db

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ const {
     USER
 } = process.env;
 
-const con = mysql.createConnection({
+const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
+    // database: 'art_ref_db'
     // password: ART_REF_DB_PASS
     // port: 3306
 });
 
-con.connect(function (err) {
+
+connection.connect((err) => {
     if (err) throw err;
     console.log("Connected!");
+    connection.query('CREATE DATABASE art_ref_db', (err, result) => {
+        if (err) throw err;
+        console.log(`Databease created\n${result}`)
+    });
 });
+
+// connection.end((err) => {
+//     if (err) throw err;
+//     console.log('Connection ended. Goodbye!');
+// });
