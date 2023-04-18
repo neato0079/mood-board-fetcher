363eb5178eddaf3b05deb85a82dd0836428795cc
Matt Q <neato0079@gmail.com>
2023-04-18 16:12:13 -0700

    Create image table

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    // database: 'art_ref_db'
+    database: 'art_ref_db'
     // password: ART_REF_DB_PASS
     // port: 3306
 });
@@ -17,9 +17,10 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err;
     console.log("Connected!");
-    connection.query('CREATE DATABASE art_ref_db', (err, result) => {
+    sql_statement = 'CREATE TABLE image (img_id INT NOT NULL PRIMARY KEY, img_name VARCHAR(255), img_location VARCHAR(255) NOT NUll)'
+    connection.query(sql_statement, (err, result) => {
         if (err) throw err;
-        console.log(`Databease created\n${result}`)
+        console.log(`Image table created\n${result}`)
     });
 });
 
