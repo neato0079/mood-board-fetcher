805ece168b1d3d0466f90a3ec3c28b3dc84d9b42
Matt Q <neato0079@gmail.com>
2023-05-09 15:33:29 -0700

    remove redundant connections

--- db-helper.js
+++ db-helper.js
@@ -30,7 +30,7 @@ const insert = (table, columns, values) => {
         if (err) throw err;
         const values_new = values.map(
             element => `('${element.name}', '${element.location}')`
-            );
+        );
         console.log("Connected!");
         const sql_statement = `INSERT INTO ${table} (${columns.join(', ')}) VALUES ${values_new}`
         connection.query(sql_statement, (err, result) => {
@@ -39,6 +39,44 @@ const insert = (table, columns, values) => {
         });
     })
 }
+
+const insertWithoutConnect = (table, column, value) => {
+    // const values_new = values.map(
+    //     element => `('${element.name}', '${element.location}')`
+    // );
+    console.log("Connected!");
+    const sql_statement = `INSERT INTO ${table} (${column}) VALUES ("${value}");`
+    connection.query(sql_statement, (err, result) => {
+        if (err) throw err;
+        console.log(`record(s) inserted`);
+    });
+}
+
+// Base data includes the following: image name, image location, and artist name
+// For example the image with path 'usr/pictures/John_Art/painting.jpg' will have the following data:
+// image name: painting.jpg
+// image location: usr/pictures/John_Art/
+// artist name: John Art
+const insertBaseData = (path, aritst, img) => { 
+    // connection.query(`
+    // START TRANSACTION;
+    // INSTERT INTO test_img ('img_name', 'file_loc')
+    // VALUES ('${img}', '${path}');
+    // COMMIT;`,(err) => {
+    //     if (err) throw err;
+    //     console.log(`record(s) inserted`);
+    // })
+    connection.query(`INSERT INTO test_img (img_name, file_loc) VALUES ('${img}', '${path}');`,(err) => {
+        if (err) throw err;
+        console.log(`record(s) inserted`);
+    })
+
+    connection.query(`INSERT INTO test_artist (artist_name) SELECT ('${aritst}') WHERE NOT EXISTS (SELECT artist_name FROM test_artist WHERE artist_name='${aritst}');`, (err) => {
+        if (err) throw err;
+    })
+}
+
+// insertBaseDataTransaction('usr/pics/John_Art', 'John Art', 'painting.jpg')
 
 const search = (user_input) => {
     // user input is taken from the front end and passed into here
@@ -51,21 +89,21 @@ const columns = [
 ]
 const imgValues = [
     {
-        name: 'test_name_unique3.jpg',
+        name: 'test_name_unique12312312.jpg',
         location: 'some/test/location'
     },
     {
         name: 'test_name_unique2.jpg',
-        location: 'test_location2'
+        location: 'test_location12331'
     }
 ]
 
 const tables = [
- 'artist',
- 'genre',
- 'image',
- 'ip',
- 'key_word'   
+    'artist',
+    'genre',
+    'image',
+    'ip',
+    'key_word'
 ]
 
 // insert('image', columns, imgValues) // BECAREFUL OF DUPLICATE ENTRIES. WILL ERROR OUT
@@ -89,4 +127,9 @@ const tables = [
 //     console.log('Connection ended. Goodbye!');
 // });
 
-module.exports = {insert}
+module.exports = { 
+    insert,
+    insertWithoutConnect,
+    insertBaseData,
+    connection 
+}
