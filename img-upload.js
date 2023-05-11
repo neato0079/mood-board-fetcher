3cad09dba22c4eeecdfbc50a7a80ad250f8c90c8
Matt Q <neato0079@gmail.com>
2023-05-10 17:52:31 -0700

    clean up comments and old code

--- img-upload.js
+++ img-upload.js
@@ -2,48 +2,14 @@ const { arch } = require('os');
 const DBHelper = require('./db-helper.js')
 const fs = require('fs');
 const { connect } = require('http2');
-// const { insert } = require('./db-helper');
 
-// create a function that returns all folders in art-refs/
 const artLib = '/Users/mattbot/Pictures/art-ref/'
-const artistNames = fs.readdirSync(artLib)
-// this removes '.DS_store' from the array. idk what that even is
-artistNames.shift()
-// console.log(artistNames)
-const testfunc = () => {
-    for (artistName of artistNames) {
-        // console.log(artistName)
-        const artist_folder = fs.readdirSync(artLib + artistName)
-        const imgFilePaths = artist_folder.map(fileName => artLib + artistName + '/' + fileName)
-        imgFilePaths.shift()
-        console.log(imgFilePaths)
-    }
-}
+const testLibPath = '/Users/mattbot/Pictures/test/'
+
 // create a function that returns all files in artist folders
 // console.log(imgFilePaths)
 // create a function that inserts all images from art-refs/ into the db
 // this funciton should also insert artist names into the artist table respective to the artist folder in art-refs/
-
-/*
-(root of art library) => return {
-    root path: root path,
-    subdirectory1: filename.jpg,
-    subdirectory1: filename2.jpg,
-    subdirectory2: filename.jpg,
-}
-(art library object) => {
-    rootpath = artLibraryObject[0]
-    for e in ALO{
-        if ALO[e] != rootpath{
-            inster(rootpath+e, img_table)
-            insert(e, artist_table)
-            insert(ALO[e], img_table)
-        }
-    }
-}
-*/
-
-const testLibPath = '/Users/mattbot/Pictures/test/'
 
 const createArtLibraryObj = (libRootPath) => {
     artLibraryObj = { libRootPath: libRootPath };
@@ -77,20 +43,12 @@ const insertEntireArtLib = (library) => {
                         continue
                     }
                     const path = library['libRootPath'] + artist
-                    // DBHelper.insertWithoutConnect('test_img', 'file_loc', path)
-                    // DBHelper.insertWithoutConnect('test_img', 'img_name', img)
-                    // DBHelper.insertWithoutConnect('test_artist', 'artist_name', artist)
                     DBHelper.insertBaseData(path, artist, img)
                 }
             }
         }
     });
 }
-
-// const prodArtLib = createArtLibraryObj('/Users/mattbot/Pictures/art-ref/')
-// console.log(testArtLib)
-// console.log(testArtLib['libRootPath'])
-// insertEntireArtLib(prodArtLib)
 
 const createAssociationImgWithArtist = (library) => {
     DBHelper.connection.connect((err) => {
@@ -109,6 +67,6 @@ const createAssociationImgWithArtist = (library) => {
     });
 }
 
-const prodArtLib = createArtLibraryObj('/Users/mattbot/Pictures/art-ref/')
-createAssociationImgWithArtist(prodArtLib)
-console.log('poop')
+// const prodArtLib = createArtLibraryObj('/Users/mattbot/Pictures/art-ref/')
+// createAssociationImgWithArtist(prodArtLib)
+// console.log('poop')
