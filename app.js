ddcaa46a5517dc83aa2f5a71956073982b37c725
Matt Q <neato0079@gmail.com>
2023-05-13 22:21:31 -0700

    still learning express... can get image data from browser now

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
+const database = require('./database')
 
-const main = async() => {
-    app.get('/asdf', async(req, res) => {
-       res.send("Hello wor!dadsjkflasjdh!!!!fslkdfgsdlk;fjg!!!!!!!!");
-       console.log(`RES: \n${res}`)
-       console.log(`REQ: \n${req[0]}`)
+const main = async () => {
+    app.get('/asdf/:id', async (req, res) => {
+        const id = req.params.id
+        // res.send(`Hello wor!dadsjkflasjdh!!!!fslkdfgsdlk;fjg!!!!!!!!${id}`);
+        res.send(`${id}`)
+        
+        console.log(`RES: \n${res}`)
+        console.log(`REQ: \n${req[0]}`)
     });
 
-    app.get('/delete_image', async(req, res) => { // go to localhost/3000/delete_image to delete an image wtf how do i ueven use this
+    app.get('/deleteImage', async (req, res) => { // go to localhost:    3000/delete_image to delete an image wtf how do i ueven use this
         res.send("This will trigger a deletion of a given image??????");
-     });
-    
+    });
+
+    app.get('/getImageData/:id', async (req, res) => {
+        const id = req.params.id
+        res.send(await database.getImageData(id));
+    });
+
     app.listen(3000);
+
 
 }
 
