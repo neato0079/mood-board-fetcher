1527fb5730cb357c798884de7139505354c52321
Matt Q <neato0079@gmail.com>
2023-05-13 21:37:08 -0700

    add express experiments. trying to understand express...

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-   res.send("Hello world!");
-});
+const main = async() => {
+    app.get('/asdf', async(req, res) => {
+       res.send("Hello wor!dadsjkflasjdh!!!!fslkdfgsdlk;fjg!!!!!!!!");
+       console.log(`RES: \n${res}`)
+       console.log(`REQ: \n${req[0]}`)
+    });
 
-app.listen(3000);
+    app.get('/delete_image', async(req, res) => { // go to localhost/3000/delete_image to delete an image wtf how do i ueven use this
+        res.send("This will trigger a deletion of a given image??????");
+     });
+    
+    app.listen(3000);
+
+}
+
+main()
+
