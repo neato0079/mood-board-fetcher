d85a1bb1647e7dec4109880d7f37e8cabd449feb
Matt Q <neato0079@gmail.com>
2023-05-18 03:35:21 -0700

    get some code ready to make this a module maybe

--- public/test.js
+++ public/test.js
@@ -1,3 +1,10 @@
+// const database = require('./database/database');
+
 const wtf = () => {
     console.log('wtf')
 }
+// const idk = async() => {
+//     const image_id = Math.floor(Math.random() * 25)
+//     const imageURL = await database.getImagePath(image_id)
+//     console.log(imageURL)
+// }
