5c1640b0d6e77e495e1aca510e1e802b45cb400f
Matt Q <neato0079@gmail.com>
2023-05-18 05:32:25 -0700

    clean up comments

--- app.js
+++ app.js
@@ -5,20 +5,10 @@ const database = require('./database/database');
 const app = express();
 
 const main = async () => {
-    // app.use(express.static('database'))
     app.use(express.static('public'))
     app.use('/pics', express.static('/Users/mattbot/Pictures/art-ref'));
 
-    app.get('/asdf/:id', async (req, res) => { //endpoint
-        const id = req.params.id
-        // res.send(`Hello wor!dadsjkflasjdh!!!!fslkdfgsdlk;fjg!!!!!!!!${id}`);
-        res.send(`${id}`)
-        
-        console.log(`RES: \n${res}`)
-        console.log(`REQ: \n${req[0]}`)
-    });
-
-    app.get('/deleteImage', async (req, res) => { // go to localhost:    3000/delete_image to delete an image wtf how do i ueven use this
+    app.get('/deleteImage', async (req, res) => { 
         res.send("This will trigger a deletion of a given image??????");
     });
 
@@ -27,23 +17,13 @@ const main = async () => {
         res.send(await database.getImageData(id)); 
     });
 
-    app.get('/checklink', async (req, res) => {
-        // create file path with js function that calls db
-        // pass that path to location
-        res.send('https://expressjs.com/en/api.html#res.location')
-        console.log('worked');
-    })
-
     app.get('/gotoimage', async (req, res) => {
-        // res.send(database.testFunc())
         const image_id = Math.floor(Math.random() * 25)
         const imageURL = await database.getImagePath(image_id)
         res.send(`<div style="height: 90vh; object-fit: contain;"><img style="max-width: 100%; max-height: 100%;" src="pics/${imageURL}"></div>`)
-        // res.redirect(`/pics/${imageURL}`)
     })
 
-
-    app.use((err, req, res, next) => {
+    app.use((err, req, res, next) => { // This handles all errors
         console.error(err.stack)
         res.status(500).send('It broke...');
     });
@@ -54,12 +34,3 @@ const main = async () => {
 }
 
 main()
-
-
-// const idk = async() => {
-//     const image_id = Math.floor(Math.random() * 25)
-//     const imageURL = await database.getImagePath(image_id)
-//     console.log(imageURL)
-// }
-
-// idk();
