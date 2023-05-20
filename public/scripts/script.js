c0765fe4ac7f7960bb442e55df6c75fa99a167f3
Matt Q <neato0079@gmail.com>
2023-05-20 03:28:39 -0700

    moved stuff around and shit works. next commit might break everything. have to change to es6 maybe

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,12 @@
-// const database = require('./database/database');
+// import database from '../../database/database';
 
 const wtf = () => {
     console.log('wtf')
 }
+
+const show = () => {
+  document.getElementById("referenceImage").innerHTML = "The function fun() is triggered !"; 
+  }
 // const idk = async() => {
 //     const image_id = Math.floor(Math.random() * 25)
 //     const imageURL = await database.getImagePath(image_id)
