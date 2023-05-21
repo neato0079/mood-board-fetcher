d7bbd7f684f185883e0834ea49c6aef961405542
Matt Q <neato0079@gmail.com>
2023-05-21 06:25:12 -0700

    install and set up ejs

--- app.js
+++ app.js
@@ -3,34 +3,38 @@ const database = require('./database/database');
 // import express from 'express'
 // import * as database from './database/database.js'
 const app = express();
+app.set('view engine', 'ejs')
 
-const main = async () => {
-    app.use(express.static('public'))
-    app.use('/pics', express.static('/Users/mattbot/Pictures/art-ref'));
+app.use(express.static('public'))
+app.use('/pics', express.static('/Users/mattbot/Pictures/art-ref'));
 
-    app.get('/deleteImage', async (req, res) => { 
-        res.send("This will trigger a deletion of a given image??????");
-    });
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 
-    app.get('/getImageData/:id', async (req, res) => {
-        const id = req.params.id
-        res.send(await database.getImageData(id)); 
-    });
+app.get('/deleteImage', async (req, res) => {
+    res.send("This will trigger a deletion of a given image??????");
+});
 
-    app.get('/gotoimage', async (req, res) => {
-        const image_id = Math.floor(Math.random() * 25)
-        const imageURL = await database.getImagePath(image_id)
-        res.send(`<div style="height: 90vh; object-fit: contain;"><img style="max-width: 100%; max-height: 100%;" src="pics/${imageURL}"></div>`)
-    })
+app.get('/getImageData/:id', async (req, res) => {
+    const id = req.params.id
+    res.send(await database.getImageData(id));
+});
 
-    app.use((err, req, res, next) => { // This handles all errors
-        console.error(err.stack)
-        res.status(500).send('It broke...');
-    });
+app.get('/gotoimage', async (req, res) => {
+    const image_id = Math.floor(Math.random() * 25)
+    const imageURL = await database.getImagePath(image_id)
+    res.send(`<div style="height: 90vh; object-fit: contain;"><img style="max-width: 100%; max-height: 100%;" src="pics/${imageURL}"></div>`)
+})
 
-    app.listen(3000);
+app.use((err, req, res, next) => { // This handles all errors
+    console.error(err.stack)
+    res.status(500).send('It broke...');
+});
+
+app.listen(3000);
 
 
-}
 
-main()
+
+
