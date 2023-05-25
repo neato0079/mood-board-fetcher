4c239e991b28aff185488b1e8ba26860421c87f2
Matt Q <neato0079@gmail.com>
2023-05-25 07:55:52 -0700

    add encodeURI() to handle white space

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,16 +5,22 @@ const wtf = () => {
 const show = () => {
   document.getElementById("referenceImage").innerHTML = "The function fun() is triggered !";
 }
-
-
-
 const searchForm = document.querySelector(".artistSearch");
 
 searchForm.addEventListener("submit", function (e) {
   e.preventDefault() // This prevents the window from reloading
 
-  const formdata = new FormData(this);
-  const input = formdata.get("artisttName");
+  const formData = new FormData(this);
+  let input = formData.get("artisttName");
+  // input = input.split(',')
+  // const result = Array.from(input, value => value.trim()) // trims white space from each value in array
+  
+  input = encodeURI(input)
+
+  // uncomment the following for block to view the contents of the formdata object
+  // for (const pair of formData.entries()) {
+  //   console.log(`${pair[0]}: '${pair[1]}'`);
+  // }
 
   window.location.href = `/search_Artist_${input}`;
 });
