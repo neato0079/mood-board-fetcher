6c563912abcf9035fcaa15c91f8147177b5c07eb
Matt Q <neato0079@gmail.com>
2023-05-25 03:27:45 -0700

    Remove old code/ comments

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,21 +7,6 @@ const show = () => {
 }
 
 
-// const idk = async() => {
-//     const image_id = Math.floor(Math.random() * 25)
-//     const imageURL = await database.getImagePath(image_id)
-//     console.log(imageURL)
-// }
-// const baseURL = 'http://localhost:3000'
-// const artistValue = document.querySelector('.artistSearch')
-// // const artistInput = document.getElementById('search')
-// artistValue.addEventListener('submit', async (e) => {
-//   e.preventDefault()
-//   const formdata = new FormData(this)
-//   const input = formdata.get('aritstName')
-//   alert(input)
-// })
-
 
 const searchForm = document.querySelector(".artistSearch");
 
@@ -35,45 +20,3 @@ searchForm.addEventListener("submit", function (e) {
 });
 
 
-
-// const asdf = async (e) => {
-//   e.preventDefault()
-//   // try{
-//   //   const artistInput = await fetch(baseURL, {
-//   //     headers: {
-//   //       'Content-Type': 'application/json',
-//   //       'Accept': 'application/json'
-//   //     },
-//   //     method: 'GET'
-//   //   }).then(response => response.json())
-//   //   .then(responseData => console.log(responseData));
-
-//   //   // const res = await artistInput.json()
-//   //   // console.log(res)
-//   // } catch (error) {
-//   //   console.log(error)
-//   //   // console.error(error)
-//   // }
-//   try {
-//     const artistInput = await fetch(baseURL, {
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//       },
-//       method: 'GET'
-//     })
-
-//     const res = JSON.stringify(artistInput)
-//     console.log(res)
-
-//     // const res = await artistInput.json()
-//     // console.log(res)
-//   } catch (error) {
-//     console.log(error)
-//     // console.error(error)
-//   }
-// }
-// const asdf = async(res) => {
-//   console.log(await res)
-// }
-// artistInput.addEventListener('click', asdf)
