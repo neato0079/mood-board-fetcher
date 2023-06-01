b08d6e4d73a6eb6a6d53bcc487c404de72369063
Matt Q <neato0079@gmail.com>
2023-06-01 11:17:34 -0700

    comment out keyword search lines. not ready yet

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ app.get('/getImageData/:id', async (req, res) => {
 });
 
 app.get('/search', async (req, res) => {
-    const artistName = req.query.artistName
-    const keyWord = req.query.keyWord
-    console.log(keyWord)
+    // const artistName = req.query.artistName
+    // const keyWord = req.query.keyWord
+    // console.log(keyWord)
     // const artistName = decodeURI(req.query.artistName)
-    const paths = await database.getImagePathByArtist(artistName)
+    const paths = await database.getImagePathByArtist(req.query)
     if (paths.length == 0) {
         res.render('noResult.ejs')
         return
